f6b7773117f94375bddbd7a89ba6029a673eda4f
Federico Lanzani <federico.lanzani@outlook.com>
2023-01-02 19:11:17 +0100

    Dummy

--- site.config.ts
+++ site.config.ts
@@ -9,8 +9,8 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
+  name: '',
+  domain: '',
   author: 'Federico Lanzani',
 
   // open graph metadata (optional)
